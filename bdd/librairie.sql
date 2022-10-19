0a164c10a4ac5ef8d72d85320d77cd22b017c9a5
Angel <angeldaniel.pastorrojas@gmail.com>
2022-10-20 00:09:38 +0200

    feat: ajout de la colonne DateRetour, premier front Library

--- bdd/librairie.sql
+++ bdd/librairie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 19 oct. 2022 à 13:50
+-- Généré le : mer. 19 oct. 2022 à 21:58
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `librairies` (
   `Titre` varchar(255) NOT NULL,
   `DateEmprunt` date DEFAULT NULL,
   `EstEmpruntee` tinyint(1) NOT NULL DEFAULT '0',
+  `DateRetour` date DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
@@ -40,15 +41,15 @@ CREATE TABLE IF NOT EXISTS `librairies` (
 -- Déchargement des données de la table `librairies`
 --
 
-INSERT INTO `librairies` (`Id`, `Titre`, `DateEmprunt`, `EstEmpruntee`) VALUES
-(1, 'Le dernier message', '2022-01-03', 1),
-(2, 'Outsider', '2021-12-09', 1),
-(3, 'Le bleu du ciel', '2021-12-21', 0),
-(4, 'Le dernier lapon', '2022-01-04', 1),
-(5, 'Carrie', '2021-11-14', 0),
-(6, '1991', '2022-01-03', 1),
-(7, 'Tu fais quoi pour Noël ? Je t\'évite', '2021-12-03', 1),
-(8, 'De force', '2022-01-10', 1);
+INSERT INTO `librairies` (`Id`, `Titre`, `DateEmprunt`, `EstEmpruntee`, `DateRetour`) VALUES
+(1, 'Le dernier message', '2022-09-30', 1, NULL),
+(2, 'Outsider', '2021-12-09', 1, NULL),
+(3, 'Le bleu du ciel', '2021-12-21', 0, '2021-12-22'),
+(4, 'Le dernier lapon', '2022-10-17', 1, NULL),
+(5, 'Carrie', '2021-11-14', 0, '2021-11-15'),
+(6, '1991', '2022-10-05', 1, NULL),
+(7, 'Tu fais quoi pour Noël ? Je t\'évite', '2021-12-03', 1, NULL),
+(8, 'De force', '2022-01-10', 1, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- controllers/Home.js
+++ controllers/Home.js
@@ -1,9 +1,10 @@
 import { connection } from "../bdd/app.js";
 
 export default (req, res) => {
-  let requete = "SELECT * From librairies";
+  let dateDiff = 21 // 3 semaines
+  let requete = "SELECT *, DATEDIFF(CURRENT_DATE,DateEmprunt)>=? as EstDepassee FROM librairies ORDER BY EstEmpruntee DESC, EstDepassee ASC, DateEmprunt DESC";
 
-  connection.query(requete, function (error, books, fields) {
+  connection.query(requete,dateDiff, function (error, books, fields) {
       !!error?console.log(error):res.render("home.ejs", { books: books });
   });
 };
